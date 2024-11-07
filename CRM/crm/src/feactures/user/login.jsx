8a44aa48eeed9c6d210ffce03e76b3a9458bc857
rushikeshballa <rushikeshballa27@gmail.com>
2024-11-07 12:40:06 +0530

    asd

--- CRM/crm/src/feactures/user/login.jsx
+++ CRM/crm/src/feactures/user/login.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { useLoginMutation,} from "../../services/crmApi";
-
+import { Navigate, useNavigate } from "react-router-dom";
 function Login() {
     
     var [loginfn]=useLoginMutation();
-   
+    var navgate=useNavigate();
         function login() {
           var x={
             username:document.getElementById('a1').value,
             password:document.getElementById("a1").value
         }
         loginfn(x).then((res)=>{
-            window.localStorage.setItem('token',res.data.token)
+            window.localStorage.setItem('token',res.data.token).then((()=>{
+                if (window.localStorage.getItem('token')) {
+                    navgate('dasbord')
+                }
+              
+            }))
         })
         } 
     return (
